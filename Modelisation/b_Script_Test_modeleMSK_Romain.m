90063649360ddc3927f9e0e72399682129574827
Romain Martinez <martinez.staps@gmail.com>
2016-10-31 16:57:47 -0400

    Fix export .trc

--- Modelisation/b_Script_Test_modeleMSK_Romain.m
+++ Modelisation/b_Script_Test_modeleMSK_Romain.m
@@ -51,8 +51,13 @@ Data(trial).Qdata     = importdata([Path.importPath Alias.Qnames(trial).name]) ;
     % Importation des marqueurs
 Data(trial).T         = S2M_rbdl('Tags', Stuff.model, Data(trial).Qdata.Q2);    % Noms des essais
     % Noms des essais
+    switch lower(Alias.Qnames(trial).name(1:4))
+        case 'irss'
+Data(trial).trialname = ['SCoRE' Alias.Qnames(trial).name(12)] ;
+        case lower(Alias.sujet)
 Data(trial).trialname = Alias.Qnames(trial).name(5:11) ;
-
+    end
+    
 %% Variables pour la création de fichiers OpenSim
     % vecteur temporel (s)
 Data(trial).time      = (0:size(Data(trial).Qdata.Q2,2)-1)/Alias.frequency;
@@ -61,32 +66,6 @@ Data(trial).frame = [1:length(Data(trial).T)];
     % dimensions
 [~,Data(trial).nb_mark,Data(trial).nb_Frame] = size(Data(trial).T);
     
-%% Creation du fichier .mot (le fichier de mouvement)
-    % Matrice pour écriture fichier .mot (Attention : Q est converti en degrés)
-Data(trial).kinmat    = [Data(trial).time ; rad2deg(Data(trial).Qdata.Q2)]; 
-    % Nom du fichier 
-Path.Filemot      = [Path.exportPath Data(trial).trialname];
-    % En-tête
-confMot.header    = {'Coordinates'; 
-                  'version          = 1';
-                  sprintf('nRows    = %d',Data(trial).nb_Frame);
-                  sprintf('nColumns = %d', Alias.nDof); 
-                  'inDegrees        = yes';
-                  'Units are S.I. units (second, meters, Newtons, ...)';
-                  'Angles are in degrees'};
-    % fin de l'entête
-confMot.endheader = {'endheader'}; 
-    % Noms des colonnes
-confMot.nameddl   = horzcat('time',transpose(Alias.nameDof));
-    % fonction permettant de générer le .mot
-GenerateOpenSimMotFile(Path.Filemot ,confMot ,Data(trial).kinmat);
-    % Nettoyage Workspace
-clearvars confMot
-end
-
-    % Condition des essais (hauteur et poids)
-[Data] = getcondition(Data);
-
 %% Creation du fichier .trc (le fichier anatomique ou mouvement si on veut faire IK)
     % Headers (fichier)
 confTrc.header1 = {'PathFileType' , '4' , '(X/Y/Z)', [Data(trial).trialname '.trc']} ;
@@ -96,11 +75,10 @@ confTrc.header2 = {'DataRate' , 'CameraRate' , 'NumFrames' , 'NumMarkers' , 'Uni
 confTrc.header3 = {num2str(Alias.frequency), num2str(Alias.frequency), num2str(Data(trial).nb_Frame),...
                    num2str(Data(trial).nb_mark), 'm' , num2str(Alias.frequency) ,...
                    '0', num2str((Data(trial).nb_Frame-1)/Alias.frequency)} ;
-    % Headers (Marqueurs + temps + frame)
-Selectedmaarkers = Alias.nameTags ; % A MODIFIER SELON MODÈLE 
+    % Headers (Marqueurs + temps + frame);
 confTrc.header4 = {'Frame#','s'};
 for i=1:Data(trial).nb_mark
-    confTrc.header4 = [confTrc.header4 Selectedmaarkers(i) ' ' ' '];
+    confTrc.header4 = [confTrc.header4 Alias.nameTags(i) ' ' ' '];
 end
     % Headers (Marqueurs + temps + frame)
 confTrc.header5 = {'Frame#','s'} ;
@@ -108,11 +86,12 @@ for i=1:Data(trial).nb_mark
     confTrc.header5 = [confTrc.header5 sprintf('x%d',i) sprintf('y%d',i) sprintf('z%d',i)];
 end
     % Matrice pour écriture fichier .trc    
-Data(trial).trcmat    = [Data(trial).frame ; Data(trial).time ; reshape(Data(trial).T,[length(Selectedmaarkers)*3 length(Data(trial).T)])]; 
+Data(trial).trcmat    = [Data(trial).frame ; Data(trial).time ; reshape(Data(trial).T,[length(Alias.nameTags)*3 length(Data(trial).T)])]'; 
     % fonction permettant de générer le .trc
-GenerateOpenSimTrcFile(Path.Filemot,confTrc,Data(trial).trcmat);
+GenerateOpenSimTrcFile(Path.exportPath, Data(trial).trialname,confTrc,Data(trial).trcmat);
     % Nettoyage Workspace
 clearvars confTrc
-%% Fermeture
+end
 
-S2M_rbdl('delete', modele_dynamique);
+    % Condition des essais (hauteur et poids)
+[Data] = getcondition(Data);
