14e0ca79058734392fcbf411afeffb2e4119b2e7
Romain Martinez <martinez.staps@gmail.com>
2016-11-18 16:43:23 -0500

    Add btk

--- OrgData/Organizer.m
+++ OrgData/Organizer.m
@@ -6,7 +6,7 @@
 % |_| \_\___/|_| |_| |_|\__,_|_|_| |_| |_|  |_|\__,_|_|   \__|_|_| |_|\___/___|  %
 %                                                                                %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                            clc; clear; close all; tic;
+                            clc; clear; close all;
 %% Chargement des fonctions
     if isempty(strfind(path, '\\10.89.24.15\e\Projet_IRSST_LeverCaisse\Codes\Functions_Matlab'))
         % Librairie S2M
@@ -19,7 +19,7 @@
     % 2) Copier l'intégralité des fichiers C3D
     
 %% Edition de fichiers : renommer les C3D
-    % 1) 
+        run('\\10.89.24.15\e\Projet_IRSST_LeverCaisse\Codes\Kinematics\OrgData\C3D2MOtoNMS.m')
 
 %% Interface d'acquisition (description des données
     % 1) Créer les SetupFiles suivant : 
@@ -28,4 +28,8 @@
         % c) EMGsProtocols
     % Ces fichiers sont créés selon le modèle XML fourni
     % 2) Interface d'acquisition
+        cd('\\10.89.24.15\e\Projet_IRSST_LeverCaisse\Codes\Functions_Matlab\MOtoNMS\src\AcquisitionInterface')
         run('\\10.89.24.15\e\Projet_IRSST_LeverCaisse\Codes\Functions_Matlab\MOtoNMS\src\AcquisitionInterface\mainAcquisitionInterface.m')
+%% Convertir les C3D en format Matlab
+        cd('\\10.89.24.15\e\Projet_IRSST_LeverCaisse\Codes\Functions_Matlab\MOtoNMS\src\C3D2MAT_btk')
+        run('\\10.89.24.15\e\Projet_IRSST_LeverCaisse\Codes\Functions_Matlab\MOtoNMS\src\C3D2MAT_btk\C3D2MAT.m')
