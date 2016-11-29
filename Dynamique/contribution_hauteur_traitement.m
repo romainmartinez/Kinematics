5887b8ad2025a2221412e24d02161aac38bf34bf
Romain Martinez <martinez.staps@gmail.com>
2016-11-29 18:25:18 -0500

    First try

--- Dynamique/contribution_hauteur_traitement.m
+++ Dynamique/contribution_hauteur_traitement.m
@@ -24,4 +24,26 @@ end
 path.datapath = '\\10.89.24.15\e\\Projet_IRSST_LeverCaisse\ElaboratedData\contribution_hauteur\elaboratedData_mat\';
 
 %% Chargement des données
-alias.matname = dir([path.datapath '*mat'])
+alias.matname = dir([path.datapath '*mat']);
+
+for i = 1 : length(alias.matname)
+sujets(i) = load([path.datapath alias.matname(i).name]);
+end
+
+%% Facteurs
+for s = 1 : length(sujets)
+sexe(1,i)
+end
+
+subject = 4;
+
+sujets(subject).data(1).deltahand
+
+plot(sujets(subject).data(1).deltahand') ; hold on
+plot(sujets(subject).data(1).deltaGH')
+plot(sujets(subject).data(1).deltaSCAC')
+plot(sujets(subject).data(1).deltaRoB')
+vline([Data(i).start/20 Data(i).end/20],{'g','r'},{'Début','Fin'})
+legend('contrib hand','contrib GH','contrib SCAC','contrib RoB')
+figure
+
