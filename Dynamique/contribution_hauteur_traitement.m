36cf9ea731bd44ce8aa4e386ffaf5f6787c39d4e
Romain Martinez <martinez.staps@gmail.com>
2016-12-02 16:08:11 -0500

    First try for SPM tests

--- Dynamique/contribution_hauteur_traitement.m
+++ Dynamique/contribution_hauteur_traitement.m
@@ -19,7 +19,9 @@ if isempty(strfind(path, '\\10.89.24.15\e\Librairies\S2M_Lib\'))
     % Librairie S2M
     loadS2MLib;
 end
-
+%% Interrupteur
+plot = 0;
+poids = 'absolu'
 %% Dossiers
 path.datapath = '\\10.89.24.15\e\\Projet_IRSST_LeverCaisse\ElaboratedData\contribution_hauteur\elaboratedData_mat\';
 
@@ -43,6 +45,9 @@ poids   = vertcat(bigstruct(:).poids);
 %% Variables
 % nombre de frames désirés pour interpolation
 nbframe = 200;
+
+% Transformation des données vers GRAMM friendly
+
 for i = 1 : length(bigstruct)
     % Transformation en cellules (pour input SPM et gramm)
     % Les essais sont découpés avec le capteur de force
@@ -59,39 +64,69 @@ for i = 1 : length(bigstruct)
 end
 
 % Vecteur X (temps en %)
-time = linspace(0,100,nbframe);
+time  = linspace(0,100,nbframe);
+
 
 clearvars bigstruct i 
 %% Plot
-% Delta hand
-g(1,1)=gramm('x',time,'y',delta_hand,'color',sexe);
-g(1,1).geom_line();
-% g(1,1).stat_summary('type','std');
-g(1,1).set_names('x','Normalized time (% of trial)','y','Contribution to the height (% of max height)','color','Sex');
-g(1,1).set_title('Contribution of the hand on the height');
+if plot == 1
+    % Delta hand
+    g(1,1)=gramm('x',time,'y',delta_hand,'color',sexe);
+    g(1,1).geom_line();
+    % g(1,1).stat_summary('type','std');
+    g(1,1).set_names('x','Normalized time (% of trial)','y','Contribution to the height (% of max height)','color','Sex');
+    g(1,1).set_title('Contribution of the hand on the height');
+    
+    % Delta GH
+    g(1,2)=gramm('x',time,'y',delta_GH,'color',sexe);
+    g(1,2).geom_line();
+    % g(1,2).stat_summary('type','std');
+    g(1,2).set_names('x','Normalized time (% of trial)','y','Contribution to the height (% of max height)','color','Sex');
+    g(1,2).set_title('Comparison of GH on the height');
+    
+    % Delta SCAC
+    g(2,1)=gramm('x',time,'y',delta_SCAC,'color',sexe);
+    g(2,1).geom_line();
+    % g(2,1).stat_summary('type','std');
+    g(2,1).set_names('x','Normalized time (% of trial)','y','Contribution to the height (% of max height)','color','Sex');
+    g(2,1).set_title('Comparison of SC & AC on the height');
+    
+    % Delta RoB
+    g(2,2)=gramm('x',time,'y',delta_RoB,'color',sexe);
+    g(2,2).geom_line();
+    % g(2,2).stat_summary('type','std');
+    g(2,2).set_names('x','Normalized time (% of trial)','y','Contribution to the height (% of max height)','color','Sex');
+    g(2,2).set_title('Comparison of the rest of the body on the height');
+    
+    g.draw();
+end
+%% SPM
+% Transformation des données
+for i = 1 : length(delta_hand)
+    % Variables en colonnes
+    SPM.delta_hand(i,:) = delta_hand{i,1}';
+    SPM.delta_GH(i,:)   = delta_GH{i,1}';
+    SPM.delta_SCAC(i,:) = delta_SCAC{i,1}';
+    SPM.delta_RoB(i,:)  = delta_RoB{i,1}';
+    
+    % Facteurs
+    if     sexe{i,1} == 'H'
+        SPM.sexe(1,i) = 0;
+    elseif sexe{i,1} == 'F'
+        SPM.sexe(1,i) = 1;
+    end
+end
 
-% Delta GH
-g(1,2)=gramm('x',time,'y',delta_GH,'color',sexe);
-g(1,2).geom_line();
-% g(1,2).stat_summary('type','std');
-g(1,2).set_names('x','Normalized time (% of trial)','y','Contribution to the height (% of max height)','color','Sex');
-g(1,2).set_title('Comparison of GH on the height');
+SPM.hauteur = hauteur';
+SPM.poids   = poids';
 
-% Delta SCAC
-g(2,1)=gramm('x',time,'y',delta_SCAC,'color',sexe);
-g(2,1).geom_line();
-% g(2,1).stat_summary('type','std');
-g(2,1).set_names('x','Normalized time (% of trial)','y','Contribution to the height (% of max height)','color','Sex');
-g(2,1).set_title('Comparison of SC & AC on the height');
+% Tests statistiques
+%(1) Conduct SPM analysis:
+spmlist   = spm1d.stats.anova3(SPM.delta_hand, SPM.sexe, SPM.hauteur, SPM.poids);
+spmilist  = spmlist.inference(0.05);
+disp_summ(spmilist)
 
-% Delta RoB
-g(2,2)=gramm('x',time,'y',delta_RoB,'color',sexe);
-g(2,2).geom_line();
-% g(2,2).stat_summary('type','std');
-g(2,2).set_names('x','Normalized time (% of trial)','y','Contribution to the height (% of max height)','color','Sex');
-g(2,2).set_title('Comparison of the rest of the body on the height');
 
-g.draw();
-
-%% test
-
+%(2) Plot:
+close all
+spmilist.plot('plot_threshold_label',false, 'plot_p_values',true, 'autoset_ylim',true);
