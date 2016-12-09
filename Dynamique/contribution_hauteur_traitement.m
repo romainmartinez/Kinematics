8ea9922ebb48b698539946b86157b6c9f25d6512
Romain Martinez <martinez.staps@gmail.com>
2016-12-09 18:12:35 -0500

    minor

--- Dynamique/contribution_hauteur_traitement.m
+++ Dynamique/contribution_hauteur_traitement.m
@@ -25,7 +25,7 @@ grammplot   =   0;                  % 0 ou 1
 plotmean    =   0;                  % 0 ou 1
 verif       =   1;                  % 0 ou 1
 stat        =   0;                  % 0 ou 1
-comparaison =  '=';                 % '=' (absolu) ou '%' (relatif)
+comparaison =  '%';                 % '=' (absolu) ou '%' (relatif)
 
 %% Dossiers
 path.datapath = '\\10.89.24.15\e\\Projet_IRSST_LeverCaisse\ElaboratedData\contribution_hauteur\elaboratedData_mat\';
