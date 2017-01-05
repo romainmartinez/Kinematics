1299eff30fd57d0c4c4ea6a0b4af5f473774161a
Romain Martinez <martinez.staps@gmail.com>
2017-01-05 15:19:23 -0800

    Passage Benjamin

--- Cinematique/functions/positionanato.m
+++ Cinematique/functions/positionanato.m
@@ -1,11 +1,11 @@
-function [q] = positionanato(Q0, DoF)
+function [q] = positionanato(Q0, model)
 GH = 22:24;
 
     function val = obj1(x)
         q = Q0;
         q(GH(1:2),:) = x;
         
-        RL = S2M_rbdl('globalJCS', Stuff.model ,q);
+        RL = S2M_rbdl('globalJCS', model ,q);
         
         zThorax = RL(1:3,3,2);
         zHum    = RL(1:3,3,5);
@@ -16,13 +16,16 @@ GH = 22:24;
         q = Q0;
         q(GH(3),:) = x;
         
-        RL = S2M_rbdl('globalJCS', Stuff.model ,q);
+        RL = S2M_rbdl('globalJCS', model ,q);
         
         xScapula = RL(1:3,1,4);
         xHum     = RL(1:3,1,5);
-        val      = -(xScapula'*xHum);
+        val      = (xScapula'*xHum);
     end
 
-xopt = lsqnonlin(obj1,[0,0])
+Q0(GH(1:2)) = lsqnonlin(@obj1,[0,0]);
+Q0(GH(3)) = lsqnonlin(@obj2,[0]);
+
+S2M_rbdl_AnimateModel(model, Q0);
 end
 
