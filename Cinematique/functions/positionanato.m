bf9cb940ad66b9c695c09ed045825225dbfab62a
Romain Martinez <martinez.staps@gmail.com>
2017-01-12 16:29:03 -0800

    Correction Mickael

--- Cinematique/functions/positionanato.m
+++ Cinematique/functions/positionanato.m
@@ -1,31 +1,42 @@
-function [q] = positionanato(Q0, model)
+function [q_optim] = positionanato(Q0, model)
+
 GH = 22:24;
+
+RL = S2M_rbdl('globalJCS', model ,Q0); 
+
+zThorax  = RL(1:3,3,2);
+xScapula = RL(1:3,1,4);
+
+options = optimoptions('lsqnonlin','Display','iter');
 
     function val = obj1(x)
         q = Q0;
         q(GH(1:2)) = x;
+
+        RL = S2M_rbdl('globalJCS', model ,q); 
         
-        RL = S2M_rbdl('globalJCS', model ,q);
-        
-        zThorax = RL(1:3,3,2);
         zHum    = RL(1:3,3,5);
         val     = -(zThorax'*zHum);
     end
 
     function val = obj2(x)
         q = Q0;
-        q(GH(3),:) = x;
-        
+        q(GH(3)) = x;
+
         RL = S2M_rbdl('globalJCS', model ,q);
-        
-        xScapula = RL(1:3,1,4);
+
+
         xHum     = RL(1:3,1,5);
-        val      = (xScapula'*xHum);
+        val      = -(xScapula'*xHum);
     end
 
-Q0(GH(1:2)) = lsqnonlin(@obj1,[0,0]);
-Q0(GH(3))   = lsqnonlin(@obj2,[0]);
+Q0(GH(1:2)) = lsqnonlin(@obj1,[0,0],[],[],options);
 
-S2M_rbdl_ShowModel(model, Q0, 'rt', true, 'comi', false, 'tags', true, 'com', false)
-axis equal
+% % verifier l'alignement
+% S2M_rbdl_ShowModel(model, Q0, 'rt', true, 'comi', false, 'tags', true, 'com', false)
+% axis equal
+
+Q0(GH(3))   = lsqnonlin(@obj2,[0],[],[],options);
+
+q_optim = Q0;
 end
