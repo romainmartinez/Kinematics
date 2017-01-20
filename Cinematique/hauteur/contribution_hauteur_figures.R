d766f33bb88846a347b0320169b24b5e39597fe6
Romain Martinez <martinez.staps@gmail.com>
2017-01-20 15:12:54 -0800

    package & load data

--- Cinematique/hauteur/contribution_hauteur_figures.R
+++ Cinematique/hauteur/contribution_hauteur_figures.R
@@ -1,4 +1,22 @@
-library(readxl)
-relative_ANOVA <- read_excel("Z:/Projet_IRSST_LeverCaisse/ElaboratedData/contribution_hauteur/SPM/relative_ANOVA.xlsx",
-                             sheet = "anova",
-                             na = "NA")
+#   Description:
+#       contribution_hauteur_traitement is used to generate figures
+#   Output:
+#       contribution_hauteur_traitement gives figures in pdf, svg and Rdata
+#   Functions:
+#       contribution_hauteur_traitement uses functions present in \\10.89.24.15\e\Project_IRSST_LeverCaisse\Codes\Functions_Matlab
+#
+#   Author:  Romain Martinez
+#   email:   martinez.staps@gmail.com
+#   Website: https://github.com/romainmartinez
+#_____________________________________________________________________________
+rm(list = ls())
+
+# Packages ----------------------------------------------------------------
+lapply(c("tidyr","dplyr","ggplot2", "readxl"), require, character.only = T)
+
+
+# Load data ---------------------------------------------------------------
+anova <- read_excel("Z:/Projet_IRSST_LeverCaisse/ElaboratedData/contribution_hauteur/SPM/relative_ANOVA.xlsx",
+                             sheet = "anova", na = "NA")
+posthoc <- read_excel("Z:/Projet_IRSST_LeverCaisse/ElaboratedData/contribution_hauteur/SPM/relative_ANOVA.xlsx",
+                    sheet = "posthoc", na = "NA")
