fa85a3298a20c3ba360146ddffabac685b4adbe6
Romain Martinez <martinez.staps@gmail.com>
2017-01-20 14:05:53 -0800

    Create R script to generate fig

---
+++ Cinematique/hauteur/contribution_hauteur_figures.R
@@ -0,0 +1,4 @@
+library(readxl)
+relative_ANOVA <- read_excel("Z:/Projet_IRSST_LeverCaisse/ElaboratedData/contribution_hauteur/SPM/relative_ANOVA.xlsx",
+                             sheet = "anova",
+                             na = "NA")
