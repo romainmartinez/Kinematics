a9341fe80c20854d2c636dca8ded40eb1ab79706
Romain Martinez <martinez.staps@gmail.com>
2017-01-26 17:06:45 -0800

    add contrib

--- Cinematique/vitesse/contribution_vitesse_traitement.m
+++ Cinematique/vitesse/contribution_vitesse_traitement.m
@@ -32,8 +32,11 @@ path.Datapath = '\\10.89.24.15\e\\Projet_IRSST_LeverCaisse\ElaboratedData\matric
 path.exportpath = '\\10.89.24.15\e\\Projet_IRSST_LeverCaisse\ElaboratedData\contribution_articulation\SPM\';
 alias.matname = dir([path.Datapath '*mat']);
 
-%% Nom des sujets
+%% Info sur le sujets et le model
 alias.sujet = sujets_valides;
+[segmentMarkers, segmentDoF] = segment_RBDL(round(model));
+segmentDoF = struct2cell(segmentDoF);
+
 
 for isujet = 1 %length(Alias.sujet) : -1 : 1
     % Open model
@@ -41,18 +44,31 @@ for isujet = 1 %length(Alias.sujet) : -1 : 1
     model = S2M_rbdl('new',Path.DirModels);
     
     % load data
-    load([path.Datapath alias.sujet{isujet} '.mat']);
+    load([path.Datapath alias.sujet{isujet} '.mat']);data = temp;clearvars temp
     
     % close model
     S2M_rbdl('delete', Alias.model);
 end
 
 %% Zone de test
-itrial = 1;
-for iframe = 1 : length(Data(itrial).Qdata.Q2)
-    TJi = S2M_rbdl('TagsJacobian', alias.model, Data(itrial).Qdata.Q2);
-    TJi  = reshape(TJi,[3,28,43]);
-    TJ(:,:,iframe) = TJi(:,:,39);
+for itrial = 1 : length(data)
+    for iframe = 1 : length(data(itrial).Qdata.Q2)
+        for isegment = 1 : 4
+            % Calcul de la jacobienne pour le frame
+            TJi = S2M_rbdl('TagsJacobian', model, data(itrial).Qdata.Q2(:,iframe));
+            % calcul de la contribution de chaque segment à la vitesse du marqueur
+            % 3 car Z ; 39 car main ; 43 + 43 + 39 --> x + y + z du marqueur 39
+            contrib(isegment,iframe) = multiprod(TJi(43+43+39,segmentDoF{isegment}),data(itrial).Qdata.QDOT2(segmentDoF{isegment},iframe));
+        end
+    end
+    data(itrial).contrib_HELB = contrib(1,:);
+    data(itrial).contrib_GH   = contrib(2,:);
+    data(itrial).contrib_SCAC = contrib(3,:);
+    data(itrial).contrib_RoB  = contrib(4,:);
+    clearvars contrib
 end
 
-vGH = multiprod(TJ(:,22:24,:), Data(itrial).Qdata.QDOT2(22:24,:))
+plot(data(1).contrib_HELB(round(data(1).start):round(data(2).end)))
+hold on
+plot(data(2).contrib_HELB(round(data(2).start):round(data(2).end)))
+plot(data(3).contrib_HELB(round(data(3).start):round(data(2).end)))
