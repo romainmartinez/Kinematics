4357a5a30e8eb0b24240d46197c45be7ee322f97
Romain Martinez <martinez.staps@gmail.com>
2017-01-27 18:06:48 -0800

    Correction Mickael

--- Cinematique/contribution/contribution_vitesse_preparation.m
+++ Cinematique/contribution/contribution_vitesse_preparation.m
@@ -23,53 +23,84 @@ end
 cd('C:\Users\marti\Documents\Codes\Kinematics\Cinematique\functions');
 
 %% Interrupteurs
-saveresults = 0;
-test        = 0;
+saveresults = 1;
 model       = 2.1;
 
 %% Dossiers
-path.Datapath = '\\10.89.24.15\e\\Projet_IRSST_LeverCaisse\ElaboratedData\matrices\cinematique\';
-path.exportpath = '\\10.89.24.15\e\\Projet_IRSST_LeverCaisse\ElaboratedData\contribution_articulation\SPM\';
-alias.matname = dir([path.Datapath '*mat']);
+path.datapath   = '\\10.89.24.15\e\\Projet_IRSST_LeverCaisse\ElaboratedData\matrices\cinematique\';
+path.exportpath = '\\10.89.24.15\e\Projet_IRSST_LeverCaisse\ElaboratedData\matrices\vitesse\';
+alias.matname = dir([path.datapath '*mat']);
 
 %% Info sur le sujets et le model
 alias.sujet = sujets_valides;
 [segmentMarkers, segmentDoF] = segment_RBDL(round(model));
 segmentDoF = struct2cell(segmentDoF);
 
-
-for isujet = 1 %length(Alias.sujet) : -1 : 1
+for isujet = 1 %length(alias.sujet) : -1 : 1
+    
+    disp(['Traitement de: ' alias.sujet{isujet} ' (' num2str(length(alias.sujet) - isujet+1) ' sur ' num2str(length(alias.sujet)) ')'])
+    
     % Open model
     Path.DirModels  = ['\\10.89.24.15\f\Data\Shoulder\Lib\' alias.sujet{isujet} 'd\Model_' num2str(round(model)) '\' 'Model.s2mMod'];
-    model = S2M_rbdl('new',Path.DirModels);
+    alias.model = S2M_rbdl('new',Path.DirModels);
     
     % load data
-    load([path.Datapath alias.sujet{isujet} '.mat']);data = temp;clearvars temp
+    load([path.datapath alias.sujet{isujet} '.mat']);data = temp;clearvars temp
+    
+    for itrial = length(data) : - 1 : 1
+        disp([9 'essai: ' num2str(length(data)-itrial+1) ' sur ' num2str(length(data))])
+        
+        for iframe = length(data(itrial).Qdata.Q2) : -1 : 1
+             TJi = S2M_rbdl('TagsJacobian', alias.model, data(itrial).Qdata.Q2(:,iframe));
+             
+            for isegment = 4 : -1 : 1
+                % jacobian for this frame
+                
+                % contribution of each segment to the vertical velocity of marker 39
+                % 43 + 43 + 39 --> x + y + z of marker 39
+                contrib(isegment,iframe) = multiprod(TJi(43+43+39,segmentDoF{isegment}),...
+                    data(itrial).Qdata.QDOT2(segmentDoF{isegment},iframe));
+            end
+            contrib(5,iframe) = TJi(43+43+39,:)*data(itrial).Qdata.QDOT2(:,iframe);
+        end
+        % low pass 15 Hz
+        data(itrial).deltahand = lpfilter(contrib(1,round(data(itrial).start):round(data(itrial).end))', 15, 100);
+        data(itrial).deltaGH   = lpfilter(contrib(2,round(data(itrial).start):round(data(itrial).end))', 15, 100);
+        data(itrial).deltaSCAC = lpfilter(contrib(3,round(data(itrial).start):round(data(itrial).end))', 15, 100);
+        data(itrial).deltaRoB  = lpfilter(contrib(4,round(data(itrial).start):round(data(itrial).end))', 15, 100);
+        data(itrial).velocity  = lpfilter(contrib(5,round(data(itrial).start):round(data(itrial).end))', 15, 100);
+        clearvars contrib
+    end
     
     % close model
-    S2M_rbdl('delete', Alias.model);
+    S2M_rbdl('delete', alias.model);
+    
+    % save matrix
+    if saveresults == 1
+        save([path.exportpath alias.sujet{isujet} '.mat'],'data')
+    end
+    clearvars data TJi 
 end
 
 %% Zone de test
-for itrial = 1 : length(data)
-    for iframe = 1 : length(data(itrial).Qdata.Q2)
-        for isegment = 1 : 4
-            % Calcul de la jacobienne pour le frame
-            TJi = S2M_rbdl('TagsJacobian', model, data(itrial).Qdata.Q2(:,iframe));
-            % calcul de la contribution de chaque segment à la vitesse du marqueur
-            % 3 car Z ; 39 car main ; 43 + 43 + 39 --> x + y + z du marqueur 39
-            contrib(isegment,iframe) = multiprod(TJi(43+43+39,segmentDoF{isegment}),...
-                                                 data(itrial).Qdata.QDOT2(segmentDoF{isegment},iframe));
-        end
-    end
-    data(itrial).contrib_HELB = contrib(1,:);
-    data(itrial).contrib_GH   = contrib(2,:);
-    data(itrial).contrib_SCAC = contrib(3,:);
-    data(itrial).contrib_RoB  = contrib(4,:);
-    clearvars contrib
-end
+tata = 1
+tp=data(tata).deltaRoB;
+plot(tp) ; hold on
+tp=tp+data(tata).deltaSCAC;
+plot(tp) ;
+tp=tp+data(tata).deltaGH;
+plot(tp) ;
+tp=tp+data(tata).deltahand;
+plot(tp) ;
 
-plot(data(1).contrib_HELB(round(data(1).start):round(data(2).end)))
-hold on
-plot(data(2).contrib_HELB(round(data(2).start):round(data(2).end)))
-plot(data(3).contrib_HELB(round(data(3).start):round(data(2).end)))
+figure
+tp=data(tata).deltaRoB;
+plot(tp) ; hold on
+tp=data(tata).deltaSCAC;
+plot(tp) ;
+tp=data(tata).deltaGH;
+plot(tp) ;
+tp=data(tata).deltahand;
+plot(tp) ;
+tp=data(tata).velocity; % la somme des autres doit être égal à elle
+plot(tp) ;
