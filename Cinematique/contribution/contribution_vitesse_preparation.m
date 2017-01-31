372bee732d188d270509e68629bcb404de3c36a0
Romain Martinez <martinez.staps@gmail.com>
2017-01-31 14:22:18 -0800

    fix typo

--- Cinematique/contribution/contribution_vitesse_preparation.m
+++ Cinematique/contribution/contribution_vitesse_preparation.m
@@ -23,7 +23,7 @@ end
 cd('C:\Users\marti\Documents\Codes\Kinematics\Cinematique\functions');
 
 %% Interrupteurs
-saveresults = 1;
+saveresults = 0;
 model       = 2.1;
 
 %% Dossiers
@@ -36,7 +36,7 @@ alias.sujet = sujets_valides;
 [segmentMarkers, segmentDoF] = segment_RBDL(round(model));
 segmentDoF = struct2cell(segmentDoF);
 
-for isujet = 1 %length(alias.sujet) : -1 : 1
+for isujet = 23%length(alias.sujet) : -1 : 1
     
     disp(['Traitement de: ' alias.sujet{isujet} ' (' num2str(length(alias.sujet) - isujet+1) ' sur ' num2str(length(alias.sujet)) ')'])
     
@@ -45,13 +45,13 @@ for isujet = 1 %length(alias.sujet) : -1 : 1
     alias.model = S2M_rbdl('new',Path.DirModels);
     
     % load data
-    load([path.datapath alias.sujet{isujet} '.mat']);data = temp;clearvars temp
+    load([path.datapath alias.sujet{isujet} '.mat']);
     
-    for itrial = length(data) : - 1 : 1
-        disp([9 'essai: ' num2str(length(data)-itrial+1) ' sur ' num2str(length(data))])
+    for itrial = length(temp) : - 1 : 1
+        disp([9 'essai: ' num2str(length(temp)-itrial+1) ' sur ' num2str(length(temp))])
         
-        for iframe = length(data(itrial).Qdata.Q2) : -1 : 1
-             TJi = S2M_rbdl('TagsJacobian', alias.model, data(itrial).Qdata.Q2(:,iframe));
+        for iframe = length(temp(itrial).Qdata.Q2) : -1 : 1
+             TJi = S2M_rbdl('TagsJacobian', alias.model, temp(itrial).Qdata.Q2(:,iframe));
              
             for isegment = 4 : -1 : 1
                 % jacobian for this frame
@@ -59,16 +59,16 @@ for isujet = 1 %length(alias.sujet) : -1 : 1
                 % contribution of each segment to the vertical velocity of marker 39
                 % 43 + 43 + 39 --> x + y + z of marker 39
                 contrib(isegment,iframe) = multiprod(TJi(43+43+39,segmentDoF{isegment}),...
-                    data(itrial).Qdata.QDOT2(segmentDoF{isegment},iframe));
+                    temp(itrial).Qdata.QDOT2(segmentDoF{isegment},iframe));
             end
-            contrib(5,iframe) = TJi(43+43+39,:)*data(itrial).Qdata.QDOT2(:,iframe);
+            contrib(5,iframe) = TJi(43+43+39,:)*temp(itrial).Qdata.QDOT2(:,iframe);
         end
         % low pass 15 Hz
-        data(itrial).deltahand = lpfilter(contrib(1,round(data(itrial).start):round(data(itrial).end))', 15, 100);
-        data(itrial).deltaGH   = lpfilter(contrib(2,round(data(itrial).start):round(data(itrial).end))', 15, 100);
-        data(itrial).deltaSCAC = lpfilter(contrib(3,round(data(itrial).start):round(data(itrial).end))', 15, 100);
-        data(itrial).deltaRoB  = lpfilter(contrib(4,round(data(itrial).start):round(data(itrial).end))', 15, 100);
-        data(itrial).velocity  = lpfilter(contrib(5,round(data(itrial).start):round(data(itrial).end))', 15, 100);
+        temp(itrial).deltahand = lpfilter(contrib(1,round(temp(itrial).start):round(temp(itrial).end))', 15, 100);
+        temp(itrial).deltaGH   = lpfilter(contrib(2,round(temp(itrial).start):round(temp(itrial).end))', 15, 100);
+        temp(itrial).deltaSCAC = lpfilter(contrib(3,round(temp(itrial).start):round(temp(itrial).end))', 15, 100);
+        temp(itrial).deltaRoB  = lpfilter(contrib(4,round(temp(itrial).start):round(temp(itrial).end))', 15, 100);
+        temp(itrial).velocity  = lpfilter(contrib(5,round(temp(itrial).start):round(temp(itrial).end))', 15, 100);
         clearvars contrib
     end
     
@@ -77,30 +77,31 @@ for isujet = 1 %length(alias.sujet) : -1 : 1
     
     % save matrix
     if saveresults == 1
-        save([path.exportpath alias.sujet{isujet} '.mat'],'data')
+        temp = rmfield(temp, {'Qdata'});
+        save([path.exportpath alias.sujet{isujet} '.mat'],'temp')
     end
-    clearvars data TJi 
+    clearvars temp TJi 
 end
 
-%% Zone de test
-tata = 1
-tp=data(tata).deltaRoB;
+% %% Zone de test
+tata = 4
+tp=temp(tata).deltaRoB;
 plot(tp) ; hold on
-tp=tp+data(tata).deltaSCAC;
+tp=tp+temp(tata).deltaSCAC;
 plot(tp) ;
-tp=tp+data(tata).deltaGH;
+tp=tp+temp(tata).deltaGH;
 plot(tp) ;
-tp=tp+data(tata).deltahand;
+tp=tp+temp(tata).deltahand;
 plot(tp) ;
-
+% 
 figure
-tp=data(tata).deltaRoB;
+tp=temp(tata).deltaRoB;
 plot(tp) ; hold on
-tp=data(tata).deltaSCAC;
+tp=temp(tata).deltaSCAC;
 plot(tp) ;
-tp=data(tata).deltaGH;
+tp=temp(tata).deltaGH;
 plot(tp) ;
-tp=data(tata).deltahand;
+tp=temp(tata).deltahand;
 plot(tp) ;
-tp=data(tata).velocity; % la somme des autres doit être égal à elle
+tp=temp(tata).velocity; % la somme des autres doit être égal à elle
 plot(tp) ;
