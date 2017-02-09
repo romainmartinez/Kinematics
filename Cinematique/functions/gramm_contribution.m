fadf448837b82f08292ccd1f7f189aa7265540a3
Romain Martinez <martinez.staps@gmail.com>
2017-02-09 15:45:25 -0800

    update gramm with new height

--- Cinematique/functions/gramm_contribution.m
+++ Cinematique/functions/gramm_contribution.m
@@ -1,27 +1,46 @@
 function gramm_contribution(input)
+idx = find(input.poids == 1);
 %% reshaping data
-sexe     = repmat(input.sexe,[1 4]);
-hauteur  = repmat(input.hauteur,[1 4]);
-delta    = kron(transpose(1:4), ones(length(input.sexe),1))';
-data  = vertcat(input.delta_hand, input.delta_GH, input.delta_SCAC, input.delta_RoB);
-time = input.time;
+long.sexe     = repmat(input.sexe(idx),[1 4]);
+long.hauteur  = repmat(input.hauteur(idx),[1 4]);
+long.delta    = kron(transpose(1:4), ones(length(input.sexe(idx)),1))';
+data.data  = vertcat(input.delta_hand(idx,:), input.delta_GH(idx,:),...
+    input.delta_SCAC(idx,:), input.delta_RoB(idx,:));
+
+for i = 1 : length(long.hauteur)
+    if  long.hauteur(i) == 1 | long.hauteur(i) == 2 | long.hauteur(i) == 4
+        data.sens{i} = 'up';
+    elseif long.hauteur(i) == 3 | long.hauteur(i) == 5 | long.hauteur(i) == 6
+        data.sens{i} = 'down';
+    end
+end
+
+% convert to string
+convert.sexe = {'men','women'};
+convert.hauteur = {'hips-shoulder','hips-eyes','hips-shoulder','shoulders-eyes','hips-eyes','shoulders-eyes'};
+convert.delta = {'hand + elbow','GH','SCAC','RoB'};
+
+data.sexe    = convert.sexe(long.sexe);
+data.hauteur = convert.hauteur(long.hauteur);
+data.delta   = convert.delta(long.delta);
+data.time    = input.time;
+
 
 % create figure
 figure('units','normalized','outerposition',[0 0 1 1])
 clear g
 
 % aes
-g = gramm('x', time ,'y', data, 'color', delta, 'linestyle', sexe);
+g = gramm('x', data.time ,'y', data.data, 'color', data.delta, 'linestyle', data.sexe);
 % facet
-g.facet_grid(hauteur,[], 'scale', 'independent','space','free');
+g.facet_grid(data.hauteur,data.sens, 'scale', 'fixed','space','free');
 % geom
 g.stat_summary('type','std','geom','area', 'setylim', true);
 % options
 g.axe_property('TickDir','out');
 % titles
-g.set_names()
+g.set_names('column','','row','','x','time (% trial)','y','contribution (% height)','color','Contribution','linestyle','sex');
 
 g.draw();
-
 end
 
