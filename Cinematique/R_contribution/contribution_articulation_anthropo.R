d8270a1e9bdabbadb841dfc4ff65fdb931dbecdd
Romain Martinez <martinez.staps@gmail.com>
2017-02-10 16:08:49 -0800

    add weight, height (mean & std)

--- Cinematique/R_contribution/contribution_articulation_anthropo.R
+++ Cinematique/R_contribution/contribution_articulation_anthropo.R
@@ -5,27 +5,32 @@
 #   Website: https://github.com/romainmartinez
 #_____________________________________________________________________________
 
-
-# Load data ---------------------------------------------------------------
+# Packages
 lapply(c("tidyr", "dplyr", "readxl", "magrittr", "knitr"),
        require,
        character.only = T)
+# path
+setwd("C:/Users/marti/Documents/Codes/Kinematics/Cinematique/R_contribution/")
 
+# Load data ---------------------------------------------------------------
 IRSST <- read_excel(
     "Z:/Projet_IRSST_LeverCaisse/ElaboratedData/contribution_articulation/IRSST_infos_sujets.xlsx",
     sheet = "Global")
 
-
 # Reshape data ------------------------------------------------------------------
+IRSST[, c(5:6)] <- sapply(IRSST[, c(5:6)], as.numeric)
 men <- IRSST %>% 
-  filter(Sexe == 'H')
+  filter(Sexe == 'H' & Traitement == 'x')
 women <- IRSST %>% 
-  filter(Sexe == 'F')
+  filter(Sexe == 'F' & Traitement == 'x')
 
 # Parameters ------------------------------------------------------------------
-# Numbers
-number.m <- men %>% filter(Traitement == 'x')
-number.w <- women %>% filter(Traitement == 'x')
-# Weight
-# Height
+anthropo <- data.frame(number = c(men %>% nrow,women %>% nrow),
+                       mean.weight = c(mean(men$Poids), mean(women$Poids)),
+                       std.weight = c(sd(men$Poids), sd(women$Poids)),
+                       mean.height = c(mean(men$Taille),mean(women$Taille)),
+                       std.height = c(sd(men$Taille), sd(women$Taille))) 
 
+anthropo[,-1] <- anthropo[,-1] %>% round(digits = 2)
+
+saveRDS(anthropo, "output/anthropo.rds")
