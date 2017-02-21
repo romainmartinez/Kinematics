352b3ee97c366ece1eada8ea0ca4009e38e77e90
Romain Martinez <martinez.staps@gmail.com>
2017-02-21 09:34:58 -0800

    scale free | space fixed

--- Cinematique/functions/gramm_contribution.m
+++ Cinematique/functions/gramm_contribution.m
@@ -28,7 +28,7 @@ clear g
 % aes
 g = gramm('x', data.time ,'y', data.data, 'color', data.delta, 'linestyle', data.sexe);
 % facet
-g.facet_grid(data.hauteur,data.sens, 'scale', 'fixed','space','free');
+g.facet_grid(data.hauteur,data.sens, 'scale', 'free','space','fixed');
 % geom
 g.stat_summary('type','std','geom','area', 'setylim', true);
 % options
