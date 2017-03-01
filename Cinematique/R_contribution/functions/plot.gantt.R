5923589a69a0d09c0defc549068f749efe4c4b86
Romain Martinez <martinez.staps@gmail.com>
2017-03-01 11:30:42 -0800

    update plot ratio

--- Cinematique/R_contribution/functions/plot.gantt.R
+++ Cinematique/R_contribution/functions/plot.gantt.R
@@ -34,10 +34,10 @@ plot.gantt <- function(data1, data2, path.output, scale.free, save.fig) {
                                             high = "deepskyblue2")
   # Theme
   gantt1 <- gantt1 + theme_classic() +
-    theme(panel.border = element_rect(colour = "black", fill = NA, size = 0.5)) + 
-    theme(text = element_text(size = 12)) + 
+    theme(panel.border = element_rect(colour = "black", fill = NA, size = 0.5)) +
+    theme(text = element_text(size = 12)) +
     theme(legend.key = element_rect(colour = NA),legend.position = "top")
-    
+
   # main effect -------------------------------------------------------------
   gantt2 <- ggplot(data2, aes())
   # create segment
@@ -70,9 +70,9 @@ plot.gantt <- function(data1, data2, path.output, scale.free, save.fig) {
 # combine -----------------------------------------------------------------
 gantt <- plot_grid(gantt2 + theme(legend.position = "none",axis.title.x = element_blank(),axis.title.y = element_blank()),
                    gantt1 + theme(legend.position = "none",axis.title.x = element_blank(),axis.title.y = element_blank()),
-                   nrow = 2, labels = "AUTO", label_size = 14, rel_heights  = c(1,4))
-legend_top <- get_legend(gantt1 + theme(legend.position = "bottom"))
-gantt <- plot_grid(legend_top, gantt, ncol = 1, rel_heights = c(0.09, 1))
+                   nrow = 2, rel_heights  = c(1,4))
+legend_top <- get_legend(gantt1 + theme(legend.position = "top"))
+gantt <- plot_grid(legend_top, gantt, ncol = 1, rel_heights = c(0.15, 1))
 # ggdraw(add_sub(gantt,
 #                "normalized time (% of trial)", vpadding = grid::unit(0,"lines"),
 #                y = 5, x = 0.5, vjust = 4.5))
